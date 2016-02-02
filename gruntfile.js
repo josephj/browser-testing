cc139007b92a1f90faebc3af544a52c7f1c73e67
josephj <josephj6802@gmail.com>
2016-02-02 22:24:51 +1100

    Fix webserver issue

--- gruntfile.js
+++ gruntfile.js
@@ -4,9 +4,15 @@ module.exports = function (grunt) {
         connect: {
             local: {
                 options: {
-                    port: 80,
+                    port: 8080,
                     base: './'
-                },
+                }
+            },
+            travis_ci: {
+                options: {
+                    port: 8080,
+                    base: './'
+                }
             }
         },
         webdriver: {
@@ -22,7 +28,7 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-webdriver');
     grunt.loadNpmTasks('grunt-contrib-connect');
 
-    grunt.registerTask('test:local', ['connect', 'webdriver:local']);
-    grunt.registerTask('test:saucelabs', ['connect', 'webdriver:saucelabs']);
+    grunt.registerTask('test:local', ['connect:local', 'webdriver:local']);
+    grunt.registerTask('test:saucelabs', ['connect:travis_ci', 'webdriver:saucelabs']);
     grunt.registerTask('default', ['test:local']);
 };
